965ec3bc6c6c0f05d9943137e89e263b5a1c887e
unknown <dbrudne1@gmail.com>
2019-01-30 19:06:03 -0600

    converting to tabs. I forgot to setup prettier

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import "@babel/polyfill";
 import "./style/style.scss";
+
+(() => {
+	const playerForm = document.querySelector("#player-name-form");
+	const fetchURL =
+		"https://infinite-cove-44078.herokuapp.com/stats?name=Kevin+Durant";
+
+	fetch(fetchURL).then(res => {
+		const x = res.json();
+		console.log(x);
+	});
+
+	// const fetchPlayer = async player => {
+	//     const response = await fetch(`https://infinite-cove-44078.herokuapp.com/stats?name=${player}`)
+	//     console.log(await response.json());
+	// }
+
+	playerForm.addEventListener("submit", e => {
+		e.preventDefault();
+		console.log(e);
+		fetchPlayer("Kevin Durant");
+	});
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            minify: true,
-            template: "./src/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "./src/style.css",
-        })
-    ],
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                // fallback to style-loader in development
-                process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader"
-            ]
-        },
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }
-        ]
-    },
-}
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			minify: true,
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "./src/style.css",
+		}),
+	],
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					// fallback to style-loader in development
+					process.env.NODE_ENV !== "production"
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+};
