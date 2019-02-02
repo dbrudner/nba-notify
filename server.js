ec0a658aad6b8e6ce90f08b2d6ebd9bd23d6cb76
unknown <dbrudne1@gmail.com>
2019-02-02 09:11:03 -0600

    update for sw

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + "/dist"));
 
+// Send service-worker before html
+app.get("/service-worker.js", (_, res) => {
+	res.sendFile(path.resolve(__dirname, "/dist", "service-worker.js"));
+});
+
 // send the user to index html page inspite of the url
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
 	res.sendFile(path.resolve(__dirname + "/dist", "index.html"));
 });
 
