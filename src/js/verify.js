ed65bb3384ef7776392452edf0bcc40ee7910e05
unknown <dbrudne1@gmail.com>
2019-02-09 14:18:12 -0600

    api key verification

--- src/js/verify.js
+++ src/js/verify.js
@@ -1,11 +1,19 @@
+import "@babel/polyfill";
 import "../style/verify.scss";
+import axios from "axios";
 
-console.log("HI");
 (() => {
-	document.querySelector(".api-key-form").addEventListener("submit", e => {
-		console.log("HI");
+	const verifyKey = async key => {
+		const res = await axios.get(
+			`http://nba-notify-api.herokuapp.com/verify?key=${key}`,
+		);
+		const data = await res.data;
+		console.log(data);
+	};
+
+	document.querySelector("#api-key-form").addEventListener("submit", e => {
 		e.preventDefault();
-		const input = document.querySelector(".api-key-input").value;
-		console.log(input);
+		const input = document.querySelector("#api-key-input").value;
+		verifyKey(input);
 	});
 })();
